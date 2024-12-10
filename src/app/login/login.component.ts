6fcd2945a8b136b7c6a71876aadb3b0f31f7663d
GJKelly75 <george.kelly@mweb.co.za>
2024-12-10 18:27:00 +0200

    Initial commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,41 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+  standalone: true,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  imports: [
+    MatCardModule, 
+    MatInputModule, 
+    MatButtonModule, 
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatIconModule
+  ]
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+  hidePassword = true;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Login form submitted', this.loginForm.value);
+    }
+  }
+}
